16d7c7b3f4814b610950fe59720fdd23e5279a66
M-Elshwadfy <mohamed.elshwadfy98@gmail.com>
2025-09-04 04:09:13 +0200

    Update pydanticai_math.py

--- pydanticai_math.py
+++ pydanticai_math.py
@@ -6,21 +6,22 @@ import logfire
 # Konfiguration des Loggings zur Analyse der Kommunikation zwischen Agent, Modell und Tool
 logfire.configure()
 logfire.instrument_pydantic_ai()
-logfire.instrument_httpx(capture_all=True)
 
 # Ollama-Provider und Modell konfigurieren
 provider = OpenAIProvider(base_url="http://localhost:11434/v1")
-model = OpenAIModel("granite3.3:8b", provider=provider)
+model = OpenAIModel("qwen3:8b", provider=provider)
 
 # Agent mit Modell initialisieren
-agent = Agent(model=model)
+agent = Agent(model=model, instructions="Use Tool evaluate_expression() for calculations")
 
 @agent.tool_plain
 def evaluate_expression(expression: str) -> float:
     """Evaluate a basic math expression"""
     allowed_names = {"__builtins__": {}}  # Zugriff auf eingebaute Funktionen sperren
-    return eval(expression, allowed_names)
+     # eval() wertet den String-Ausdruck aus und liefert das numerische Ergebnis
+    return eval(expression, allowed_names) 
 
 # Beispielanfrage an den Agenten
-response = agent.run_sync("Calculate 8*9*2*3/1.34")
-print(response.output)
+user_prompt = "What is 545.38*74.62/6.83?"
+result = agent.run_sync(user_prompt)
+print(result.output)
